3e5823e0860cf4dc627d432891153bfce5cf6473
non0n0no <a55120@alunos.ipb.pt>
2024-06-12 01:56:05 +0100

    Lesson 3.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,12 +11,15 @@ public class PlayerController : MonoBehaviour
     bool isOnGround = true;
     public bool gameOver = false;
 
+    private Animator playerAnim;
+
 
     // Start is called before the first frame update
     void Start()
     {
         
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
         
 
@@ -25,10 +28,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -43,6 +47,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
 
         }
     }
